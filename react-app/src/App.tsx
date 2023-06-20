d682f09c567a104e0ac517d35353e3d4dc8f25be
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-21 01:31:37 +0700

    23-exercise alert

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Alert</Alert>
+      )}
+
       <Button
         // color="primary"
         children="Button"
-        onClick={() => console.log("Ok")}
+        onClick={() => setAlertVisibility(true)}
       />
     </div>
   );
