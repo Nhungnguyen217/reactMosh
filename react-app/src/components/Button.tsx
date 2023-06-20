b15d63f16cb3736d917de76a0a324435dc8fa14e
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-20 21:33:02 +0700

    change

--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,9 +1,9 @@
 interface Props {
   children: string;
-  color?: string; // "?" khong can khai bao ben App.tsx
+  color?: "dark" | "primary"; // "?" khong can khai bao ben App.tsx, chi co the chon 2 mau o day
   onClick: () => void;
 }
-const Button = ({ children, onClick, color = "dark" }: Props) => {
+const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
     <button type="button" className={"btn btn-" + color} onClick={onClick}>
       {children}
