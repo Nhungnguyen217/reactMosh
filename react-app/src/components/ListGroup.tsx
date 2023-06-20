ad3d468cdda5093fd9235998aa4f791a7a11bd18
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-20 10:09:57 +0700

    16-Managing State

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,18 +1,28 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "Paris", "Tokyo", "Ha Noi", "Da Nang"];
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1); // const [name, setNam] = useState('')
+  // arr[0] : varriable; arr[1]: update function, update varriable
 
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
