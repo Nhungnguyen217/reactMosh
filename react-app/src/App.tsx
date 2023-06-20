05929b4ae9a6b314bbe5db8d1be54a85e2542752
Nhungnguyen217 <63488905+Nhungnguyen217@users.noreply.github.com>
2023-06-20 21:31:28 +0700

    22-exxercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,13 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>world</span>{" "}
-      </Alert>
+      <Button
+        // color="primary"
+        children="Button"
+        onClick={() => console.log("Ok")}
+      />
     </div>
   );
 }
--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,7 +1,12 @@
-const Button = () => {
+interface Props {
+  children: string;
+  color?: string; // "?" khong can khai bao ben App.tsx
+  onClick: () => void;
+}
+const Button = ({ children, onClick, color = "dark" }: Props) => {
   return (
-    <button type="button" className="btn btn-dark">
-      Dark
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
     </button>
   );
 };
